ca8baaee32be2f3249de120c4936553ded8ab602
baligabhavatha <61545941+baligabhavatha@users.noreply.github.com>
2026-04-07 14:32:46 +0530

    Update esp32_mqtt_arduino_ide.ino

--- esp32_mqtt_arduino_ide.ino
+++ esp32_mqtt_arduino_ide.ino
@@ -3,8 +3,8 @@
 #include "DHT.h"
 
 // WiFi
-const char* ssid = <WIFI Username>;
-const char* password = <WIFI Password>;
+const char* ssid = WIFI username;
+const char* password = WIFI password;
 
 // MQTT
 const char* mqtt_server = "broker.hivemq.com";
@@ -118,86 +118,6 @@ void loop() {
   } else {
     Serial.println("Publish failed");
   }
-
-  delay(3000);
-}
-const char* password = <wifi password>;
-
-// MQTT
-const char* mqtt_server = "broker.hivemq.com";
-
-// DHT setup
-#define DHTPIN 4
-#define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-void setup_wifi() {
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("\nWiFi connected");
-}
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Connecting to MQTT...");
-    if (client.connect("ESP32_DHT11")) {
-      Serial.println("connected");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      delay(2000);
-    }
-  }
-}
-
-void setup() {
-  Serial.begin(115200);
-  dht.begin();
-
-  setup_wifi();
-
-  client.setServer(mqtt_server, 1883);
-}
-
-void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
-  float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
-
-  if (isnan(humidity) || isnan(temperature)) {
-    Serial.println("Failed to read from DHT!");
-    delay(2000);
-    return;
-  }
-
-  // JSON payload
-  String payload = "{";
-  payload += "\"temp\":" + String(temperature) + ",";
-  payload += "\"humidity\":" + String(humidity);
-  payload += "}";
-
-  Serial.println(payload);
-
-  // Publish
-  //client.publish("esp32/dht11", payload.c_str());
-  if (client.publish("esp32/dht11/bhavatha", payload.c_str())) {
-  Serial.println("Published to MQTT");
-} else {
-  Serial.println(" Publish failed");
-}
 
   delay(3000);
 }
